1b2e8ae76f5652519a7b64b973e2e7033678e9bb
morgvanny <morgvanny@gmail.com>
2016-06-04 20:38:25 -0600

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(flavor) {
+  return 'I love ' + flavor;
+}
+
+function shouting(stuff) {
+  return stuff.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
